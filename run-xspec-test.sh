b54479fcc39a2b927643a76f80953e105090354b
Fred <f.atherden@elifesciences.org>
2019-09-02 13:00:28 +0100

    Add test files

---
+++ run-xspec-test.sh
@@ -0,0 +1,12 @@
+for xspectest in *.xspec;
+do /tmp/xspec/bin/xspec.sh $xspectest &> result.log; 
+if grep -q ".*failed:\s[1-9]" result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" result.log;
+then
+echo "FAILED: $xspectest";
+echo "---------- result.log";
+cat result.log;
+echo "----------";
+exit 1;
+else echo "OK: $xspectest";
+fi
+done
