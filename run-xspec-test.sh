4cfdce31a12824fc9c04e53c8e73bc5fa9711325
Fred <f.atherden@elifesciences.org>
2019-09-02 15:40:40 +0100

    update travis and shell

--- run-xspec-test.sh
+++ run-xspec-test.sh
@@ -1,5 +1,5 @@
 for xspectest in *.xspec;
-do /tmp/xspec/bin/xspec.sh $xspectest &> result.log; 
+do /tmp/xspec/bin/xspec.sh $xspectest &> result.log;
 if grep -q ".*failed:\s[1-9]" result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" result.log;
 then
 echo "FAILED: $xspectest";
