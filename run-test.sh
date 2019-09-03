19e0726367d6cdb516fd941825881d76c5ab4bc4
Fred <f.atherden@elifesciences.org>
2019-09-03 09:17:38 +0100

    Update test

---
+++ run-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export SAXON_HOME=~/tmp/saxon
+for xspectest in xspec/*.xspec;
+do ~tmp/xspec/bin/xspec.sh -s $xspectest &> result.log
+    if grep -q ".*failed:\s[1-9]" result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" result.log;
+        then
+            echo "FAILED: $xspectest";
+            echo "---------- result.log";
+            cat result.log;
+            echo "----------";
+            exit 1;
+        else echo "OK: $xspectest";
+    fi
+done
