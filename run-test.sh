9e4c50c7b3fa9f95d21fc37d49ef7cc4689d8388
NuclearRedeye <j.summerfield@elifesciences.org>
2020-05-18 18:06:18 +0100

    fix: update tests to use submodule

--- run-test.sh
+++ run-test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-
-#export SAXON_HOME=/home/travis/tmp/saxon
-echo "SAXON_HOME=$GITHUB_WORKSPACE"
-echo "SAXON_HOME=$SAXON_HOME"
 for xspectest in test/xspec/*.xspec;
-do /home/travis/tmp/xspec/bin/xspec.sh -s $xspectest &> result.log
+do ./xspec/bin/xspec.sh -s $xspectest &> result.log
     if grep -q ".*failed:\s[1-9]" result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" result.log;
         then
             echo "FAILED: $xspectest";
