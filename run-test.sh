ceaaa4065cb39456da90ffa506b54e4ce6650ea4
NuclearRedeye <j.summerfield@elifesciences.org>
2020-05-18 17:46:23 +0100

    chore: add basic test target

--- run-test.sh
+++ run-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export SAXON_HOME=/home/travis/tmp/saxon
+#export SAXON_HOME=/home/travis/tmp/saxon
+echo "SAXON_HOME=$SAXON_HOME"
 for xspectest in test/xspec/*.xspec;
 do /home/travis/tmp/xspec/bin/xspec.sh -s $xspectest &> result.log
     if grep -q ".*failed:\s[1-9]" result.log || grep -q -E "\*+\sError\s(running|compiling)\sthe\stest\ssuite" result.log;
